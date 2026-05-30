a1fa95ccda487270dc86aa1b18d6f5e849971458
venkkkatt <venkkat764@gmail.com>
2026-05-30 12:22:02 +0530

    axios.js

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import useAuthStore from "../store/authStore";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: ["http://localhost:5000/api", import.meta.env.VITE_API_URL],
   withCredentials: true,
 });
 
