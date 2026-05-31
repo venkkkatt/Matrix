cb6655f623fe27485f604394a80ee335c509674f
venkkkatt <venkkat764@gmail.com>
2026-05-31 15:11:36 +0530

    production env 2

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -26,8 +26,8 @@ const uploadToCloudinary = (buffer) => {
 
 const cookieOptions = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  secure: true,
+  sameSite:"none",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
 
